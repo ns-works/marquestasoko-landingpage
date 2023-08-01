c0e96f3804a0d130051ec3029ee4fc82dae40e40
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-31 23:35:03 -0300

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+        colors: {
+            'primary': '#252223',
+            'secondary': '#A78043',
+        },
+        padding: {
+            'small': '2rem',
+            'medium': '5rem',
+            'large': '7rem',
+            'global': '10rem',
+        },
+        },
+    },
+};
